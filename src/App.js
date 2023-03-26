aa4370242feedf6a824b3732dabaa8b665bc34b5
hamoudafeki <hamouda125.fh@gmail.com>
2023-03-26 21:36:27 +0100

    seconde commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@
 
 function App() {
   return (
-    <div>
-      <h1 className="solo">solo making</h1>
-      <h2>keep goining</h2>
-      <h3>last branch</h3>
+    <>
+
       
-    </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+import Home from "./pages/Home";
+import Html from "./pages/Html";
+import Css from "./pages/Css";
+import Javascript from "./pages/Javascript";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <h1>NF404</h1>,
+  },
+
+  {
+    path: "/html",
+    element: <Html />,
+  },
+
+  {
+    path: "/css",
+    element: <Css />,
+  },
+
+  {
+    path: "/javascript",
+    element: <Javascript />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />{" "}
   </React.StrictMode>
 );
-
-
